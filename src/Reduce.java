4ebda96b54068849ac7e960d41ba6588fef2898b
milabhaloo <milabhaloo@mail.utoronto.ca>
2023-09-13 11:39:07 -0400

    update

---
+++ src/Reduce.java
@@ -0,0 +1,16 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int count = 0;
+        while(n != 0){
+            count += 1;
+            if (n % 2 == 0){
+                n = n/2;
+            }
+            else{
+                n = n-1;
+            }
+        }
+        System.out.println(count);
+    }
+}
