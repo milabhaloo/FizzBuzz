3a20397d5deff06ae8220f828c87f36ee719e000
milabhaloo <milabhaloo@mail.utoronto.ca>
2023-09-13 11:26:27 -0400

    new changes'

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,14 @@
 public class Multiples {
+    public static void main(String[] args){
+        int counter = 0;
+        for(int i = 1; i<1000; i++){
+            if(i % 3 == 0){
+                counter += 1;
+            }
+            else if(i % 5 == 0){
+                counter += 1;
+            }
+        }
+        System.out.println(counter);
+    }
 }
